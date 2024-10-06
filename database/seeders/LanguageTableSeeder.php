b03c1c463cb70ea57e748019b8508994cd918055
My-projects-dev <anar.ismayilov.1991@inbox.ru>
2024-10-07 00:49:05 +0400

    z

--- database/seeders/LanguageTableSeeder.php
+++ database/seeders/LanguageTableSeeder.php
@@ -14,6 +14,7 @@ class LanguageTableSeeder extends Seeder
     {
         \DB::table('languages')->delete();
 
+
         \DB::table('languages')->insert(array (
             0 =>
                 array (
